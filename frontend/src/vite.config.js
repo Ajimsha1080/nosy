5b610dc60c0d8ea5ff94ae2f64bb0e82b3d1ed41
Ajimsha1080 <147695569+Ajimsha1080@users.noreply.github.com>
2025-10-19 00:02:43 +0530

    Update vite.config.js

--- frontend/src/vite.config.js
+++ frontend/src/vite.config.js
@@ -1,1 +1,10 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
+export default defineConfig({
+  plugins: [react()],
+  root: '.', // frontend folder root
+  build: {
+    outDir: 'dist', // default output folder
+  },
+});
